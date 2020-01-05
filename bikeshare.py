07f6f0c56c32603801177152100470c335cad742
Terry Milyard <tmilyard@gmail.com>
2020-01-05 14:11:57 -0600

    edited while loops

--- bikeshare.py
+++ bikeshare.py
@@ -20,7 +20,7 @@ def get_filters():
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
     i = 1
-    while i < 999:
+    while i < 888:
         city = input("Choose a city: 'chicago', 'new york city' or 'washington':    ").lower()
         valid_cities = ("chicago","new york city","washington")
         if city in valid_cities:
@@ -31,7 +31,7 @@ def get_filters():
 
     # TO DO: get user input for month (all, january, february, ... , june)
     i = 1
-    while i < 999:
+    while i < 888:
         month = input("Choose a month:  'january', 'february', 'march', 'april', 'may', 'june', or 'all':    ").lower()
         valid_months = ("january", "february", "march", "april", "may", "june", "all")
         if month in valid_months:
@@ -43,7 +43,7 @@ def get_filters():
 
     # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
     i = 1
-    while i < 999:
+    while i < 888:
         day = input("Choose a day:  'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', or 'all':    ").lower()
         valid_days = ("monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday", "all")
         if day in valid_days:
