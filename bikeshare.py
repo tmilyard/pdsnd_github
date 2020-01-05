cc7cdd4069ab99a7835d7ad42bc7ae0185640dca
Terry Milyard <tmilyard@gmail.com>
2020-01-05 13:58:23 -0600

    small bikeshare comment added

--- bikeshare.py
+++ bikeshare.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 import numpy as np
 
+"""Adding additional text and documentation changes"""
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
